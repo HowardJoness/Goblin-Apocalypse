73a4c6752ae7884a76aae092b52dacbe3aa10e25
HowardJoness <143377834+HowardJoness@users.noreply.github.com>
2025-09-08 16:44:48 +0800

    增加了一个地图框架

--- scripts/map_choice.gd
+++ scripts/map_choice.gd
@@ -13,6 +13,18 @@ func add_map():
 	item_instance.map_image_path = "res://srcs/images/容器 20@1x.png"
 	item_instance.difficuty = 1
 
+func add_map2():
+	var item_instance = MapChoiceScene.instantiate()
+	var hbox = $ScrollContainer/HBoxContainer
+	hbox.add_child(item_instance)
+	
+	# ✅ 设置实例化对象的属性
+	item_instance.map_name = "州音街 502号"
+	item_instance.map_description = "我们收容，我们控制，我们保护。你好，这里是教学管理处，我们收容一些不听话的学生。包括，你。"
+	item_instance.target_scene_path = "uid://btb3qq41j5jv8"
+	item_instance.map_image_path = "res://srcs/images/容器 20@1x.png"
+	item_instance.difficuty = 2
+
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		SceneManager.change_scene("uid://gk5a8awxsh3w")
@@ -20,3 +32,4 @@ func _input(event):
 func _ready() -> void:
 	# 进入地图选择场景后的准备工作
 	add_map()
+	add_map2()
